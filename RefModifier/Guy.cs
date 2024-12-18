db0648c698c616c5a53b530f9417efe5fbbf85ca
Bartek <bartlomiej.kacki@gmail.com>
2024-12-18 12:01:40 +0100

    Add project files.

---
+++ RefModifier/Guy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefModifier
+{
+    internal class Guy
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public override string ToString() => $"{Age}-latek o imieniu {Name}"; 
+    }
+}
---
+++ RefModifier/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace RefModifier
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var i = 1;
+            var guy = new Guy() { Name="Julian", Age=26 };
+            Console.WriteLine("i to {0}, a guy to {1}", i, guy);
+            ModifyAnIntAndGuy(ref i, ref guy);
+            Console.WriteLine("Teraz i to {0}, a guy to {1}", i, guy);
+        }
+
+        static void ModifyAnIntAndGuy(ref int valueRef, ref Guy guyRef)
+        {
+            valueRef += 10;
+            guyRef.Name = "Bogdan";
+            guyRef.Age = 33;
+        }
+    }
+}
